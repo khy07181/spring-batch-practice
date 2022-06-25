b51f2c64d61e44a1d4447c644c3924b5053d8705
khy07181 <khy07181@gmail.com>
2022-06-25 16:54:14 +0900

    initial commit

---
+++ src/test/java/com/example/springbatchpractice/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
